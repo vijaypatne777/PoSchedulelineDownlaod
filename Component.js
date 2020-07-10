8b7e2800db83643472dd0b3483172ade71377e85
S0021768522 <aritra.gupta1@wipro.com>
2020-07-10 07:34:13 +0000

    Import content for version 1.0.2

---
+++ Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","YY1_PO_DATE_214/YY1_PO_DATE_214/model/models"],function(e,t,i){"use strict";return e.extend("YY1_PO_DATE_214.YY1_PO_DATE_214.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);this.getRouter().initialize();this.setModel(i.createDeviceModel(),"device")}})});
---
+++ test/integration/opaTests.qunit-dbg.js
@@ -0,0 +1,12 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"YY1_PO_DATE_214/YY1_PO_DATE_214/test/integration/AllJourneys"
+	], function () {
+		QUnit.start();
+	});
+});
